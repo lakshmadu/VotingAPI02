d11d17432877b169a73402f1fb19b5fe37a50c7e
Lakshan Madhubashika <LakshanMadhubashika@horizoncampus.edu.lk>
2021-12-04 23:55:09 +0530

    added jwt step01

--- VotingAPI/Controllers/CandidateController.cs
+++ VotingAPI/Controllers/CandidateController.cs
@@ -7,22 +7,25 @@ using VotingAPI.Models;
 using VotingAPI.Services.Candidates;
 using AutoMapper;
 using VotingAPI.Services.Models;
-
+using Microsoft.AspNetCore.Authorization;
+using VotingAPI.Services.Security;
 
 namespace VotingAPI.Controllers
 {
-
+    [Authorize]
     [Route("api/candidate")]
     [ApiController]
     public class CandidateController : Controller
     {
         private readonly ICandidateRepository _service;
         private readonly IMapper _mapper;
+        private readonly IJWTAuthenticationManager _jwtAuthenticationManager;
         
-        public CandidateController(ICandidateRepository service, IMapper mapper)
+        public CandidateController(ICandidateRepository service, IMapper mapper, IJWTAuthenticationManager jWTAuthenticationManager)
         {
             _service = service;
             _mapper = mapper;
+            _jwtAuthenticationManager = jWTAuthenticationManager;
         }
 
         
@@ -59,6 +62,18 @@ namespace VotingAPI.Controllers
 
 
 
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = _jwtAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
         }
 
     }
--- VotingAPI/Program.cs
+++ VotingAPI/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using VotingAPI.DataAccess;
 using VotingAPI.Services.Candidates;
 using VotingAPI.Services.Parties;
+using VotingAPI.Services.Security;
 using VotingAPI.Services.Voters;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//public IConfiguration Configuration { get; }
 
 // Add services to the container.
 
@@ -24,6 +30,33 @@ builder.Services.AddScoped<IPartyRepository, PartySqlserverService>();
 
 builder.Services.AddScoped<ICandidateRepository, CandidateSqlServerService>();
 
+var tokenKey = configuration.GetValue<string>("TokenKey");
+
+var key = Encoding.ASCII.GetBytes(tokenKey);
+
+
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationManager(tokenKey));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,8 +67,22 @@ if (app.Environment.IsDevelopment())
     //app.UseCors(options =>
     //options.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
 }
+else
+{
+    app.UseExceptionHandler(app =>
+    {
+        app.Run(async context =>
+
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsync("There was an error in the server.Please contact developer");
+        });
+    });
+}
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
